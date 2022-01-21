dd438998d25d980ea086a8e26a40a357147db3dd
ElDeebo <a_eldeebo@yahoo.com>
2022-01-21 16:59:09 +0200

    to be countied

---
+++ models/author.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose')
+
+const authorsSchema =new mongoose.Schema({
+    name:{
+        type: String,
+        require:true
+    }
+})
+module.exports=mongoose.model('Author',authorsSchema)
