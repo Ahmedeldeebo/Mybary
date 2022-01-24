15e91d213d9432e5b502faaeeccc0b9f5ac787e3
ElDeebo <a_eldeebo@yahoo.com>
2022-01-24 16:12:46 +0200

    Initial Author Routes

--- routes/authors.js
+++ routes/authors.js
@@ -4,8 +4,22 @@ const router =express.Router()
 const Author=require('../models/author')
 
 // All Authors Route
-router.get('/',(req,res)=>{
-    res.render('authors/index')
+router.get('/', async (req,res)=>{
+    let searchOptions ={}
+    if (req.query.name != null && req.query.name != ''){
+        searchOptions.name= new RegExp(req.query.name,'i')
+    }
+    try {
+        const authors =await Author.find(searchOptions)
+        res.render('authors/index',{
+            authors:authors ,
+            searchOptions:req.query
+        })
+    } catch {
+        res.redirect('/')
+
+    }
+    
 })
   
 // New Author Route
@@ -19,17 +33,15 @@ router.post('/',async(req,res)=>{
         name:req.body.name
     })
     try{
-        const newAuthor=await author.save()
+        const newAuthor = await author.save()
          //res.redirect(`authors/${newAuthor.id}`)
-            res.redirect('authors')
+         res.redirect(`authors`)
 
     } catch{
-        res.render ('authors/new ',{
+        res.render ('authors/new',{
           author: author,
         errorMessage:'Error Creating Author'
         })
-
-
     }
 })
 
